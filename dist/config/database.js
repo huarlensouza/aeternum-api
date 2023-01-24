2be7183fac9a905ee56fb6b6278fcc84aeb75007
Huarlen Souza <huarlensouza@hotmail.com>
2023-01-24 15:41:28 -0300

    Delete dist

--- dist/config/database.js
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});
-exports. default = {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-};
--- dist/lib/Database.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _mysql2 = require('mysql2'); var _mysql22 = _interopRequireDefault(_mysql2);
-var _database = require('../config/database'); var _database2 = _interopRequireDefault(_database);
-
-exports. default = _mysql22.default.createPool(_database2.default);
